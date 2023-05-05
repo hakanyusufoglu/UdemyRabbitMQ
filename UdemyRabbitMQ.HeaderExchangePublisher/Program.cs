a7bc7f9f26ea3df0fe7b6af49b935e34203111ca
hyusufoglu <hyusufoglu@isbak.istanbul>
2023-05-05 11:25:51 +0300

    HeaderExchange Eklendi.

---
+++ UdemyRabbitMQ.HeaderExchangePublisher/Program.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace UdemyRabbitMQ.HeaderExchangePublisher
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			
+				var factory = new ConnectionFactory();
+				factory.Uri = new Uri("amqps://chgkpvyr:KkEiat6wFkGRT67kXSf2mbSlcfuuopXI@cow.rmq2.cloudamqp.com/chgkpvyr");
+
+				using var connection = factory.CreateConnection();
+
+				//Bir bağlantıya kanal channel oluşturuyoruz.
+				var channel = connection.CreateModel();
+
+				//Exchange oluşturuyoruz...
+				channel.ExchangeDeclare("header-exchange", durable: true, type: ExchangeType.Headers);
+
+				Dictionary<string, object> headers = new Dictionary<string, object>();
+				headers.Add("format", "pdf");
+				headers.Add("shape2", "a4");
+
+				var properties = channel.CreateBasicProperties();
+				properties.Headers = headers;
+				properties.Persistent = true;//mesajlar kalıcı hale getirmektedir.
+				channel.BasicPublish("header-exchange", string.Empty, properties, Encoding.UTF8.GetBytes("Header mesajim"));
+				Console.WriteLine("Mesaj gönderilmiştir");
+				Console.ReadLine();
+
+		}
+	}
+}
---
+++ UdemyRabbitMQ.HeaderExchangeSubscriber/Program.cs
@@ -0,0 +1,53 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace UdemyRabbitMQ.HeaderExchangeSubscriber
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			var factory = new ConnectionFactory();
+			factory.Uri = new Uri("amqps://chgkpvyr:KkEiat6wFkGRT67kXSf2mbSlcfuuopXI@cow.rmq2.cloudamqp.com/chgkpvyr");
+
+			using var connection = factory.CreateConnection();
+
+			//Bir bağlantıya kanal channel oluşturuyoruz.
+			var channel = connection.CreateModel();
+
+
+			//kaç kaç veri göndereceğimiz belirilyoruz.
+			//ilk parametre: bana herhangi bir boyuttaki mesajı gönderebilirsin,
+			//ikicni: her bir subcriber'e birer mesaj gelsin
+			//üçüncü: kaç subcriber varsa tek bir seferde 5 olacak şekilde ayarlar (true ise); false ise kaç tane varsa o kadar yani 2 tane subcriber varsa 5,5 gönderecektir.
+			channel.BasicQos(0, 1, false);
+
+
+			var consumer = new EventingBasicConsumer(channel);
+			var queueName = channel.QueueDeclare().QueueName;
+			Dictionary<string, object> headers = new Dictionary<string, object>();
+			headers.Add("format", "pdf");
+			headers.Add("shape", "a4");
+			headers.Add("x-match", "any"); //all dersek publisher tarafında tüm key, value değerlerinin uyuşması gerekiyor diyoruz. eğer any dersek tek bir tanesinin uyuşması yeterli oluyor.
+
+			channel.QueueBind(queueName,"header-exchange",string.Empty,headers);
+			//autoAck = Subscriber doğruda yanlışsa işlense bilgi silinir. false yaparsak sen bu bilgiyi kuyruktan silme diyoruz. Böylece emin oluyoruz.
+			channel.BasicConsume(queueName, false, consumer);
+
+			Console.WriteLine("Loglar dinleniyor...");
+			consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+			{
+				var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+				Thread.Sleep(1500);
+				Console.WriteLine("Gelen Mesaj:" + message);
+
+				channel.BasicAck(e.DeliveryTag, false);
+			};
+
+			Console.ReadLine();
+		}
+	}
+}
