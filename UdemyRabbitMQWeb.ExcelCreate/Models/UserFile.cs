1063fd269b61b4711e159ad163307f7d737c55d4
hyusufoglu <hyusufoglu@isbak.istanbul>
2023-05-08 15:07:08 +0300

    MVC projesi oluşturuldu. UserFile modeli oluşturuldu EntityFramework kütüphaneleri eklendi Seed veriler eklendi.

---
+++ UdemyRabbitMQWeb.ExcelCreate/Models/UserFile.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UdemyRabbitMQWeb.ExcelCreate.Models
+{
+    //Excel dosyası oluşturuluyor veya oluşturuldu gibi file durumlarını tutmak için bu enum oluşturuldu.
+    public enum FileStatus
+    {
+        Creating,
+        Completed
+    }
+    public class UserFile
+    {
+        public int Id { get; set; } 
+        public string UserId{ get; set; }
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public FileStatus FileStatus { get; set; }
+
+        //Veritabanında tabloya bu özelliği ekleme diyoruz.
+        [NotMapped]
+        public string GetCreatedDate => CreatedDate.HasValue ? CreatedDate.Value.ToShortDateString() : "-"; //bir tarihe sahipse tarihi döndür değilse geriye çizgi döndür
+    }
+}
---
+++ UdemyRabbitMQWeb.ExcelCreate/Program.cs
@@ -0,0 +1,77 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using UdemyRabbitMQWeb.ExcelCreate.Models;
+
+namespace UdemyRabbitMQWeb.ExcelCreate
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            
+            // Add services to the container.
+
+            //sql baðlantýsýný gerçekleþtiriyoruz.
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
+            });
+
+            //identity ekliyoruz
+            builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+            {
+                options.User.RequireUniqueEmail= true;
+            }).AddEntityFrameworkStores<AppDbContext>();
+            builder.Services.AddControllersWithViews();
+            
+            var app = builder.Build();
+
+            using(var scope=app.Services.CreateScope())
+            {
+                var appDbContext=scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                //getrequiredservice kullanmamýzýn sebebi mutlaka bu servisin olmasý gerektiðini vurgulamak için kullanýyoruz
+                var userManager=scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+                appDbContext.Database.Migrate();
+
+                //Any() kayýt var ise true döndürür
+                if (!appDbContext.Users.Any())
+                {
+                    userManager.CreateAsync(new IdentityUser()
+                    {
+                        UserName = "deneme",
+                        Email = "deneme@outlook.com"
+                    }, "Password12*").Wait();
+
+                    userManager.CreateAsync(new IdentityUser()
+                    {
+                        UserName = "deneme2",
+                        Email = "deneme2@outlook.com"
+                    }, "Password12*").Wait();
+                }
+            }
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            //identity eklediðimiz için tanýmladýk
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
