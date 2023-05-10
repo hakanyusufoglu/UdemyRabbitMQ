0ca3a3ba990724d5739e71424c4ce9ca30e230b8
hyusufoglu <hyusufoglu@isbak.istanbul>
2023-05-10 11:25:27 +0300

    Worker projesinde dinlenecek kuyruk belirtildi. MVC projesinde tüm dosyaları görüntülenebileceği Files sekmesi eklendi

--- UdemyRabbitMQWeb.ExcelCreate/Controllers/ProductsController.cs
+++ UdemyRabbitMQWeb.ExcelCreate/Controllers/ProductsController.cs
@@ -50,7 +50,7 @@ namespace UdemyRabbitMQWeb.ExcelCreate.Controllers
         public async Task<IActionResult> Files()
         {
             var user = await _userManager.FindByNameAsync(User.Identity.Name);
-            return View(await _context.UserFiles.Where(x => x.UserId == user.Id).ToListAsync());
+            return View(await _context.UserFiles.Where(x => x.UserId == user.Id).OrderByDescending(x=>x.Id).ToListAsync());
         }
     }
 }
